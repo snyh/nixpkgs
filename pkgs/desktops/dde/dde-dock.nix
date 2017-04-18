bf18434bba2ea55d3b11e83bd67db24118909b29
snyh <snyh@snyh.org>
2017-04-18 23:31:47 +0800

    dde: dde-dock init at 4.2.1

---
+++ pkgs/desktops/dde/dde-dock.nix
@@ -0,0 +1,33 @@
+{ stdenv, fetchFromGitHub,
+  qt5, pkgconfig,
+  deepin-tool-kit, libXtst,
+}:
+
+stdenv.mkDerivation rec {
+  name = "dde-dock-${version}";
+  version = "4.2.1";
+
+  src = fetchFromGitHub {
+    owner = "linuxdeepin";
+    repo = "dde-dock";
+    rev = version;
+    sha256 = "0kpia9v9k6w00vhjcwckbflsfvgb4blf8mfgkvhn5jx7qsgjiibf";
+  };
+
+  patches = [ ./patchs/fix_dde-dock.patch ];
+
+  nativeBuildInputs = [ pkgconfig qt5.qtbase qt5.qmakeHook ];
+  buildInputs = [ qt5.qttools qt5.qtmultimedia qt5.qtx11extras deepin-tool-kit libXtst ];
+
+  preConfigure = ''
+    qmakeFlags="$qmakeFlags INSTALL_ROOT=$out"
+  '';
+
+  enableParallelBuilding = true;
+
+  meta = {
+    homepage = "https://github.com/linuxdeepin/dde-dock";
+    license = stdenv.lib.licenses.gpl3;
+    platforms = stdenv.lib.platforms.linux;
+  };
+}
